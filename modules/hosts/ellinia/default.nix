865e79b501096c99f49d7d7520a8a0518f054ca6
Max Su <max@maxsu.dev>
2026-09-02 04:51:43 -0400

    add bootloader + user to ellinia config

--- modules/hosts/ellinia/default.nix
+++ modules/hosts/ellinia/default.nix
@@ -3,13 +3,26 @@
   flake.modules.nixos.ellinia = { pkgs, ... }: {
     imports = [
       ./_hardware-configuration.nix
-      self.modules.nixos.nh
     ];
 
     networking.hostName = "ellinia";
     system.stateVersion = "26.05";
 
-    programs.nh.flake = "/home/frieren/.config/nix-dendritic";
+    # Bootloader
+    boot.loader.systemd-boot.enable = false;
+    boot.loader.limine.enable = true;
+    boot.loader.efi.canTouchEfiVariables = true;
+    programs.zsh.enable = true;
+
+    users.users.frieren = {
+      isNormalUser = true;
+      home = "/home/frieren";
+      shell = pkgs.zsh; # Setting Zsh as the default shell
+      # https://github.com/savedra1/clipse#Auto-paste input
+      extraGroups = [ "wheel" "networkmanager" "input" ];
+    };
+
+    programs.nh.flake = "/home/frieren/.config/nix";
   };
 
   flake.nixosConfigurations.ellinia = inputs.nixpkgs.lib.nixosSystem {
@@ -17,6 +30,7 @@
     specialArgs = { inherit inputs self; };
     modules = [
       self.modules.nixos.ellinia
+      self.modules.nixos.nh
       inputs.home-manager.nixosModules.home-manager
       {
         home-manager = {
@@ -29,11 +43,11 @@
               self.modules.homeManager.git
               self.modules.homeManager.zsh
             ];
-            home.username = "frieren";
-            home.homeDirectory = "/home/frieren";
-            home.stateVersion = "26.05";
-            # TODO: Change this to nix
-            programs.nh.flake = "/home/frieren/.config/nix-dendritic";
+            home = {
+              username = "frieren";
+              homeDirectory = "/home/frieren";
+              stateVersion = "26.05";
+            };
           };
         };
       }
