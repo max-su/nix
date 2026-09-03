559bf90527a040c8141975f887c172209c67152d
Max Su <max@maxsu.dev>
2026-09-03 05:45:21 -0400

    comment: add comments to noctalia-req

--- modules/system/noctalia-requirements.nix
+++ modules/system/noctalia-requirements.nix
@@ -1,3 +1,11 @@
+# https://docs.noctalia.dev/noctalia/ v5
+# To make Noctalia’s wifi, bluetooth, power-profile, and battery feature available, please ensure the following NixOS options are enabled:
+#
+#     networking.networkmanager.enable
+#     hardware.bluetooth.enable
+#     services.power-profiles-daemon.enable or services.tuned.enable
+#     services.upower.enable
+
 { ... }:
 let
   noctaliaRequirements = {
