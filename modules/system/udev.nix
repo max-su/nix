5510ebc97bdea351b95ba40028f49c011ed13466
Max Su <max@maxsu.dev>
2026-09-03 13:47:58 -0400

    add comment to udev indicating it was for clipse

--- modules/system/udev.nix
+++ modules/system/udev.nix
@@ -2,6 +2,7 @@
 {
   flake.modules.nixos.base.imports = [
     {
+      # github.com/savedra1/clipse
       services.udev.extraRules = ''
         KERNEL=="uinput", MODE="777", GROUP="input", OPTIONS+="static_node=uinput"
       '';
