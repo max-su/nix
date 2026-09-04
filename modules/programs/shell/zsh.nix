ed70793e9595b2fe0cfb75a6a7ae69bd1b316e1c
Max Su <max@maxsu.dev>
2026-09-03 21:02:00 -0400

    refactor(zsh): change to nixos/homeManager convention let var in

--- modules/programs/shell/zsh.nix
+++ modules/programs/shell/zsh.nix
@@ -1,34 +1,35 @@
 { ... }:
-{
-  flake.modules.nixos.base.imports = [
-    { programs.zsh.enable = true; }
-  ];
-
-  flake.modules.homeManager.frieren.imports = [
-    ({ config, ... }: {
-      programs.zsh = {
-        enable = true;
-        enableCompletion = true;
-        autosuggestion.enable = true;
-        syntaxHighlighting.enable = true;
-        shellAliases = {
-          ll = "ls -l";
-          ga = "git add";
-          gc = "git commit";
-          gd = "git diff";
-          gp = "git push";
-          pbcopy = "wl-copy";
-          lc = "mv ~/Downloads/lc.pdf";
-        };
-        initContent = ''
-          eval "$(jump shell)"
-        '';
-        history = {
-          size = 10000;
-          path = "${config.home.homeDirectory}/.zsh_history";
-          ignoreAllDups = true;
-        };
+let
+  nixos = {
+    programs.zsh.enable = true;
+  };
+  homeManager = { config, ... }: {
+    programs.zsh = {
+      enable = true;
+      enableCompletion = true;
+      autosuggestion.enable = true;
+      syntaxHighlighting.enable = true;
+      shellAliases = {
+        ll = "ls -l";
+        ga = "git add";
+        gc = "git commit";
+        gd = "git diff";
+        gp = "git push";
+        pbcopy = "wl-copy";
+        lc = "mv ~/Downloads/lc.pdf";
       };
-    })
-  ];
+      initContent = ''
+        eval "$(jump shell)"
+      '';
+      history = {
+        size = 10000;
+        path = "${config.home.homeDirectory}/.zsh_history";
+        ignoreAllDups = true;
+      };
+    };
+  };
+in
+{
+  flake.modules.nixos.base.imports = [ nixos ];
+  flake.modules.homeManager.frieren.imports = [ homeManager ];
 }
