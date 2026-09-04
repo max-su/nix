2b9599f6d95118bc305a866c82551bd5e433af5e
Max Su <max@maxsu.dev>
2026-09-03 20:58:35 -0400

    refactor(yazi): change to let in to make more readable

--- modules/programs/shell/yazi.nix
+++ modules/programs/shell/yazi.nix
@@ -1,26 +1,22 @@
 { inputs, ... }:
-{
-  flake.modules.nixos.base.imports = [
-    {
-      nixpkgs.overlays = [
-        inputs.nix-yazi-flavors.overlays.default
-      ];
-    }
-  ];
-
-  flake.modules.homeManager.frieren.imports = [
-    ({ pkgs, ... }: {
-      home.packages = [
-        pkgs.yazi
-      ];
-      programs.yazi = {
-        enable = true;
-        flavors = pkgs.yaziFlavors;
-        theme.flavor = {
-          dark = "kanagawa";
-          light = "catppuccin-latte";
-        };
+let
+  nixos = {
+    nixpkgs.overlays = [
+      inputs.nix-yazi-flavors.overlays.default
+    ];
+  };
+  homeManager = { pkgs, ... }: {
+    programs.yazi = {
+      enable = true;
+      flavors = pkgs.yaziFlavors;
+      theme.flavor = {
+        dark = "kanagawa";
+        light = "catppuccin-latte";
       };
-    })
-  ];
+    };
+  };
+in
+{
+  flake.modules.nixos.base.imports = [ nixos ];
+  flake.modules.homeManager.frieren.imports = [ homeManager ];
 }
