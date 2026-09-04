d008536005e949bc22be602f5edec8af16a9f7fa
Max Su <max@maxsu.dev>
2026-09-03 20:49:58 -0400

    refactor(font): rm overlay

--- modules/system/fonts.nix
+++ modules/system/fonts.nix
@@ -1,27 +1,24 @@
-{ self,
+{
   inputs,
   ...
 }:
 let
-  fontsModule = { config, pkgs, ... }: {
-    nixpkgs.overlays = [ self.overlays.fonts ];
-    fonts.packages = [ pkgs.codelia-nerd-font ];
+  codeliaFont = pkgs: pkgs.stdenvNoCC.mkDerivation {
+    pname = "codelia-nerd-font";
+    version = "1.0";
+    src = inputs.codelia;
+    dontUnpack = true;
+    installPhase = ''
+      mkdir -p $out/share/fonts/opentype
+      cp $src/CodeliaNerdFont-Regular.otf $out/share/fonts/opentype/
+    '';
+  };
+
+  fontsModule = { pkgs, ... }: {
+    fonts.packages = [ (codeliaFont pkgs) ];
     fonts.fontconfig.enable = true;
   };
 in
 {
-  flake.overlays.fonts = final: prev: {
-    codelia-nerd-font = final.stdenvNoCC.mkDerivation {
-      pname = "codelia-nerd-font";
-      version = "1.0";
-      src = inputs.codelia;
-      dontUnpack = true;
-      installPhase = ''
-        mkdir -p $out/share/fonts/opentype
-        cp $src/CodeliaNerdFont-Regular.otf $out/share/fonts/opentype/
-      '';
-    };
-  };
-
   flake.modules.nixos.base.imports = [ fontsModule ];
 }
