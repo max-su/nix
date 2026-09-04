13a2255fe9edc7688e5a57705692ac6ee91bd496
Max Su <max@maxsu.dev>
2026-09-03 21:09:10 -0400

    refactor(homeManager): rm TODO comment

--- modules/system/home-manager.nix
+++ modules/system/home-manager.nix
@@ -4,13 +4,6 @@
 }:
 {
   flake.modules.homeManager.frieren = {
-    # TODO: auto discovery through imports.
-    # manual
-    # imports = [
-    #   config.flake.modules.homeManager.yazi
-    #   config.flake.modules.homeManager.zsh
-    # ];
-
     home = {
       username = "frieren";
       homeDirectory = "/home/frieren";
