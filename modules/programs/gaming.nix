9a3f0507d817a49ea2f1504d899f2717ef2ec071
Max Su <max@maxsu.dev>
2026-09-04 18:13:47 -0400

    add(mangohud): to gaming

--- modules/programs/gaming.nix
+++ modules/programs/gaming.nix
@@ -1,6 +1,6 @@
 { ... }:
-{
-  flake.modules.nixos.gaming = {
+let
+  nixos = {
     programs.steam = {
       enable = true;
       remotePlay.openFirewall = true;
@@ -10,4 +10,21 @@
     };
     programs.gamemode.enable = true;
   };
+
+  homeManager = {
+    programs.mangohud = {
+      enable = true;
+      settings = {
+        vram = true;
+        fps = true;
+        frametime = true;
+        no_display = false;
+        gpu_list = "0";
+      };
+    };
+  };
+in
+{
+  flake.modules.nixos.gaming = nixos;
+  flake.modules.homeManager.gaming = homeManager;
 }
