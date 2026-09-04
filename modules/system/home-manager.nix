bb3e3d4b247842bbf35b5f7af213bcd01764eb9d
Max Su <max@maxsu.dev>
2026-09-04 19:58:13 -0400

    feat(home-manager): mkdir -p ~/Code/saccharine

--- modules/system/home-manager.nix
+++ modules/system/home-manager.nix
@@ -1,13 +1,13 @@
+{ ... }:
 {
-  config,
-  ...
-}:
-{
-  flake.modules.homeManager.frieren = {
+  flake.modules.homeManager.frieren = { lib, ... }: {
     home = {
       username = "frieren";
       homeDirectory = "/home/frieren";
       stateVersion = "26.05";
+      activation.createSaccharineDir = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+        $DRY_RUN_CMD mkdir -p $VERBOSE_ARG "$HOME/Code/saccharine"
+      '';
     };
 
     programs.home-manager.enable = true;
