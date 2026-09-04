28bc5ad5c74d46eb515d44f0366734ac166ad68a
Max Su <max@maxsu.dev>
2026-09-03 21:13:01 -0400

    fix(nh)

--- modules/system/nh.nix
+++ modules/system/nh.nix
@@ -1,18 +1,21 @@
-{ inputs, ... }:
+{ ... }:
+let
+  nixos = {
+    programs.nh = {
+      enable = true;
+      clean.enable = true;
+      clean.extraArgs = "--keep-since 7d --keep 5";
+    };
+  };
+  homeManager = {
+    programs.nh = {
+      enable = true;
+      clean.enable = true;
+      clean.extraArgs = "--keep-since 7d --keep 5";
+    };
+  };
+in
 {
-  flake.modules.nixos.base = {
-    programs.nh = {
-      enable = true;
-      clean.enable = true;
-      clean.extraArgs = "--keep-since 7d --keep 5";
-    };
-  };
-
-  flake.modules.homeManager.nh = {
-    programs.nh = {
-      enable = true;
-      clean.enable = true;
-      clean.extraArgs = "--keep-since 7d --keep 5";
-    };
-  };
+  flake.modules.nixos.base.imports = [ nixos ];
+  flake.modules.homeManager.frieren.imports = [ homeManager ];
 }
