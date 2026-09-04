d34c19650eb34a3a511b4c2a8076ae36f6b0801b
Max Su <max@maxsu.dev>
2026-09-03 20:24:43 -0400

    fix(zsh): fix home manager

--- modules/programs/shell/zsh.nix
+++ modules/programs/shell/zsh.nix
@@ -4,32 +4,31 @@
     { programs.zsh.enable = true; }
   ];
 
-  flake.modules.homeManager.zsh = { config, ... }: {
-    programs.zsh = {
-      enable = true;
-      enableCompletion = true;
-      autosuggestion.enable = true;
-      syntaxHighlighting.enable = true;
-
-      shellAliases = {
-        ll = "ls -l";
-        ga = "git add";
-        gc = "git commit";
-        gd = "git diff";
-        gp = "git push";
-        pbcopy = "wl-copy";
-        lc = "mv ~/Downloads/lc.pdf";
+  flake.modules.homeManager.frieren.imports = [
+    ({ config, ... }: {
+      programs.zsh = {
+        enable = true;
+        enableCompletion = true;
+        autosuggestion.enable = true;
+        syntaxHighlighting.enable = true;
+        shellAliases = {
+          ll = "ls -l";
+          ga = "git add";
+          gc = "git commit";
+          gd = "git diff";
+          gp = "git push";
+          pbcopy = "wl-copy";
+          lc = "mv ~/Downloads/lc.pdf";
+        };
+        initContent = ''
+          eval "$(jump shell)"
+        '';
+        history = {
+          size = 10000;
+          path = "${config.home.homeDirectory}/.zsh_history";
+          ignoreAllDups = true;
+        };
       };
-
-      initContent = ''
-        eval "$(jump shell)"
-      '';
-
-      history = {
-        size = 10000;
-        path = "${config.home.homeDirectory}/.zsh_history";
-        ignoreAllDups = true;
-      };
-    };
-  };
+    })
+  ];
 }
