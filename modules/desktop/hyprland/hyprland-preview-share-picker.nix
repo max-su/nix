e6c737f78fc0cb6f5e6a4cc4dad2f30ae3cc993d
Max Su <max@maxsu.dev>
2026-09-04 20:05:15 -0400

    add(hyprland-preview-share-picker): add module that replaces the share screen picker

---
+++ modules/desktop/hyprland/hyprland-preview-share-picker.nix
@@ -0,0 +1,14 @@
+{ ... }:
+let
+  homeManager = {
+    xdg.configFile."hypr/xdph.conf".text = ''
+      screencopy {
+        allow_token_by_default = true
+        custom_picker_binary = hyprland-preview-share-picker
+      }
+    '';
+  };
+in
+{
+  flake.modules.homeManager.frieren.imports = [ homeManager ];
+}
