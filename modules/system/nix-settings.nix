6d24a062008c27173502f0de526705d8575983b7
Max Su <max@maxsu.dev>
2026-09-05 05:51:49 -0400

    add(cachix): add vicinae to cachix list

--- modules/system/nix-settings.nix
+++ modules/system/nix-settings.nix
@@ -3,9 +3,18 @@ let
   nixSettings = {
     nix.settings = {
       experimental-features = "nix-command flakes";
-      substituters = [ "https://hyprland.cachix.org" ];
-      trusted-substituters = [ "https://hyprland.cachix.org" ];
-      trusted-public-keys = [ "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" ];
+      substituters = [
+        "https://hyprland.cachix.org"
+        "https://vicinae.cachix.org"
+      ];
+      trusted-substituters = [
+        "https://hyprland.cachix.org"
+        "https://vicinae.cachix.org"
+      ];
+      trusted-public-keys = [
+        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+        "vicinae.cachix.org-1:1kDrfienkGHPYbkpNj1mWTr7Fm1+zcenzgTizIcI3oc="
+      ];
     };
   };
 in
