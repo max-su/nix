0ca5129f1aef5299c7836a10f1e39a7ae4ae2f6f
Max Su <max@maxsu.dev>
2026-09-23 11:13:28 -0400

    feat(hyprland): remove noctalia bar blur in hyprland

--- modules/desktop/hyprland/hyprland.nix
+++ modules/desktop/hyprland/hyprland.nix
@@ -209,6 +209,16 @@ let
           bezier = "md3_decel",
           style = "slidevert"
         })
+        hl.layer_rule({
+          name = "noctalia",
+          match = {
+            namespace = "^noctalia-(bar-.+|notification|dock|panel|attached-panel|osd|window-switcher)$",
+          },
+          no_anim = true,
+          ignore_alpha = 0.5,
+          blur = true,
+          blur_popups = true,
+        })
       '';
 
       settings = {
